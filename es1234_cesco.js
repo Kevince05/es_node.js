76f5c870daa4de954c7729e9cbc3acb8d65be72b
Kevince05 <cesco.kevin05@gmail.com>
2023-11-28 11:20:32 +0100

    repo init

---
+++ es1234_cesco.js
@@ -0,0 +1,38 @@
+const net = require("net");
+
+let cl = "";
+let buff = "";
+const sv = net.createServer((socket) => {
+  socket.on("data", (data) => {
+    buff += data;
+    if (buff.endsWith("\n")) {
+      switch (buff.trim()) {
+        case "quit":
+          socket.end();
+          break;
+        case "exit":
+          socket.end();
+          break;
+
+        default:
+          socket.write(buff);
+          buff = "";
+          break;
+      }
+    }
+  });
+  socket.on("end", () => {
+    console.log(cl + "-disconnected ");
+    buff = "";
+  })
+
+});
+
+sv.on("connection", (socket) => {
+  cl = "welcome " +
+    socket.remoteAddress == "::1" ? "localhost" : socket.remoteAddress.replace("::ffff", "") +
+    ":" + socket.remotePort;
+  socket.write(cl + "\r\n");
+});
+
+sv.listen(7979);
